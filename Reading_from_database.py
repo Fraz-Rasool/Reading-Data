7d97f963c24e2f55020752492cc93b4ad7f8c8cf
Fraz Rasool <frazrasool618@gmail.com>
2024-10-15 17:02:37 +0500

    Completed

--- Reading_from_database.py
+++ Reading_from_database.py
@@ -6,22 +6,31 @@ password=input("Enter the password:")
 database=input("Enter the name of the databse:")
 
 try:
-    conn = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-
+    if not host or not user or not password or not database:
+        print("Error! Missing some details")
+        exit()
+    else:
+        conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
     if conn.is_connected:
         print("Connection is connected")
         fcursor=conn.cursor()
-        query="select * from actor;"
+        fcursor.execute("SELECT DATABASE();")
+        name=fcursor.fetchall()
+        print(name)
+        table=input("Enter the table name form which you need data: ")
+        if not table:
+            print("Table doesn't exists in database!")
+            exit()
+        query="select* from "+table
         fcursor.execute(query)
         data=fcursor.fetchall()
     else:
         print("Couldn't connect")
-
     for row in data:
         print(row)
 except Exception as e:
